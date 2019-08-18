f5d128a6561a74384f75cfd8c2358f30d0de8956
Austin Asbury <acasbury25@aol.com>
2019-08-18 11:35:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,17 +2,20 @@
 def line (current_line)
   if current_line.size == 0
     puts "The line is currently empty."
-    return
   else
     string = "The line is currently: "
     current_line.each_with_index do |person, i|
       string << "#{i + 1}. #{person} "
     end
+    puts string.strip
   end
-  puts string.strip
 end
 
 def take_a_number (cur_line, name)
   cur_line.push(name)
   puts "Welcome, #{name}. You are number #{cur_line.size} in line."
+end
+
+def now_serving (cur_line)
+  if cur_line
 end
