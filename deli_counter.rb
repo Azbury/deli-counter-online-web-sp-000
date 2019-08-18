7d604d4cf25023683b26957f506aee18bb2ae2d2
Austin Asbury <acasbury25@aol.com>
2019-08-18 11:19:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 def line (current_line)
   if current_line.size == 0
-    puts "The line is currently is empty."
+    puts "The line is currently empty."
     return
   else
     string = "The line is currently: "
