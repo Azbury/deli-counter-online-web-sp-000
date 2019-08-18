e5ba1ee4c80bf839de65030324d4b0bc8c993936
Austin Asbury <acasbury25@aol.com>
2019-08-18 11:19:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+def line (current_line)
+  if current_line.size == 0
+    puts "The line is currently is empty."
+    return
+  else
+    string = "The line is currently: "
+    current_line.each_with_index do |person, i|
+      string << "#{i + 1}. #{person} "
+    end
+  end
+  puts string
+end
+    
